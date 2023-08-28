ef83900b3415d82087dcf4565360c674d156214a
Nicolay Moreno <nicolay.mherrera@gmail.com>
2023-08-28 18:41:04 -0500

    Carousel-Slider-Reactjs

---
+++ src/assets/data.js
@@ -0,0 +1,22 @@
+export const data = [
+   {
+      id: 1,
+      imgUrl: "src/assets/images/playStation.jpg",
+   },
+   {
+      id: 2,
+      imgUrl: "src/assets/images/xbox.jpg",
+   },
+   {
+      id: 3,
+      imgUrl: "src/assets/images/nintendo.jpg",
+   },
+   {
+      id: 4,
+      imgUrl: "src/assets/images/nintendo2.jpg",
+   },
+   {
+      id: 5,
+      imgUrl: "src/assets/images/playStation2.jpg",
+   },
+];
